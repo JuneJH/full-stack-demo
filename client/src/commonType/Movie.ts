9439eb5dc741172adb6cfa169382b179a817dd9f
JuneJH <15310487388@163.com>
2020-12-26 21:41:35 +0800

    创建增加、编辑、删除、查找接口
    Signed-off-by: JuneJH <15310487388@163.com>

---
+++ client/src/commonType/Movie.ts
@@ -0,0 +1,13 @@
+
+export interface Movie {
+     id?: number;
+     name: string;
+     types: string[];
+     areas: string[];
+     timeLong: number;
+     isHot: boolean;
+     isComing: boolean;
+     isClassic: boolean;
+     description?: string;
+     poster?: string;
+}
