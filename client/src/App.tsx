000fa102303b32f69e475ed29c9d24dc00d4c29b
JuneJH <15310487388@163.con>
2020-12-28 16:53:02 +0800

    初始化

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MovieApi from "./api/MovieApi"
+import {Movie} from "./commonType/Movie";
 
+
+const m ={
+
+
+}
+MovieApi.edit("97afbf78-04a3-4b94-b0fa-3fd15670f776",m).then(r => console.log("修改结果",r))
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        hellow word
     </div>
   );
 }
