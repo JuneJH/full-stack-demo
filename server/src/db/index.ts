033345fe0216631242ee878c3321b843b42b2afa
JuneJH <15310487388@163.com>
2021-01-07 10:14:51 +0800

    修改连接数据库方式

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,7 +1,16 @@
 import {createConnection} from "typeorm";
-
+import {Movie} from "../entities/Movie"
 async function init(){
-    const result = await createConnection();
+    console.log("run")
+    const result = await createConnection({
+        type: "mysql",
+        host: "121.36.51.141",
+        port: 3306,
+        username: "root",
+        password: "root",
+        database: "movie",
+        entities:[Movie]
+    });
     return result;
 }
 
