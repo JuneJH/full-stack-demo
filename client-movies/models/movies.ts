b32a61b5d5cd6165d71b3edda4cdf42c83374b16
JuneJH <15310487388@163.com>
2021-04-27 23:16:22 +0800

    构建模型TODO

---
+++ client-movies/models/movies.ts
@@ -0,0 +1,13 @@
+export default {
+    state: {
+        condition: {
+            page: 1,
+            take: 10,
+        },
+        data: [],
+        isLoading: false,
+        total: 0,
+        totalPage: 0,
+    },
+    
+}
