e765a5a9cab3a1c35950c43c9d2aef5716912bb3
JuneJH <15310487388@163.com>
2021-04-29 22:34:08 +0800

    电影

--- client-movies/src/layouts/index.tsx
+++ client-movies/src/layouts/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Layout, Menu } from 'antd';
+import { Layout, Menu ,Row,Col,Button} from 'antd';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
-  UserOutlined,
   VideoCameraOutlined,
   UploadOutlined,
 } from '@ant-design/icons';
-import { Link } from 'umi';
+import { Link,history } from 'umi';
+import style from './index.less'
 
 const { Header, Sider, Content } = Layout;
 
@@ -27,8 +27,24 @@ export default class SiderDemo extends React.Component<any> {
     if(pathname === "/login") return this.props.children;
     return (
       <Layout style={{height:"100%"}}>
+      
+          <Header style={{padding:0}}>
+          <div className={style.logo}>
+            <Row justify="space-between">
+              <Col>
+                <h2 style={{color:"inherit"}}>个人系统后台管理系统</h2>
+              </Col>
+              <Col>
+                <Button type="link" style={{color:"inherit"}} onClick={()=>{
+                  history.push("/login")
+                }}>退出</Button>
+              </Col>
+            </Row>
+          </div>
+          </Header>
+       
+        <Layout className="site-layout">
         <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" style={{height:60}}>后台管理系统</div>
           <Menu theme="dark" mode="inline" defaultSelectedKeys={[pathname]}>
             <Menu.Item key="/movie/list" icon={<VideoCameraOutlined />}>
               <Link to="/movie/list">电影列表</Link>
@@ -41,13 +57,6 @@ export default class SiderDemo extends React.Component<any> {
             </Menu.Item>
           </Menu>
         </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: this.toggle,
-            })}
-          </Header>
           <Content
             className="site-layout-background"
             style={{
