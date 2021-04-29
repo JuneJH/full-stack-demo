fc9bda474f0b0404c99179da03a33023b2e81973
JuneJH <15310487388@163.com>
2021-04-29 12:55:44 +0800

    获取ID

--- client-movies/src/pages/movie/add.tsx
+++ client-movies/src/pages/movie/add.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function add() {
+export default function add(props:any) {
+    console.log(props)
     return (
         <div>
-                添加
+                添加{props.match.params.id}
         </div>
     )
 }
--- client-movies/src/pages/movie/edit/[id].tsx
+++ client-movies/src/pages/movie/edit/[id].tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Detail() {
+export default function Detail(props:any) {
     return (
         <div>
-            详情页
+            详情页添加{props.match.params.id}
         </div>
     )
 }
