679f66fdb65f8d7ca233e0958ee60ff196108ec9
JuneJH <15310487388@163.com>
2021-04-29 21:25:09 +0800

    选中导航栏问题

--- client-movies/src/layouts/index.tsx
+++ client-movies/src/layouts/index.tsx
@@ -23,20 +23,21 @@ export default class SiderDemo extends React.Component<any> {
   };
 
   render() {
-    if(this.props.location.pathname === "/login") return this.props.children;
+    const pathname = this.props.location.pathname
+    if(pathname === "/login") return this.props.children;
     return (
       <Layout style={{height:"100%"}}>
         <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
           <div className="logo" style={{height:60}}>后台管理系统</div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<VideoCameraOutlined />}>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={[pathname]}>
+            <Menu.Item key="/movie/list" icon={<VideoCameraOutlined />}>
               <Link to="/movie/list">电影列表</Link>
             </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            <Menu.Item key="/movie/add" icon={<VideoCameraOutlined />}>
                <Link to="/movie/add">添加电影</Link>
             </Menu.Item>
             <Menu.Item key="3" icon={<UploadOutlined />}>
-              其他
+              学生列表
             </Menu.Item>
           </Menu>
         </Sider>
--- client-movies/src/pages/index.tsx
+++ client-movies/src/pages/index.tsx
@@ -1,9 +1,9 @@
 import styles from './index.less';
-
+import { Typography } from 'antd';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <Typography.Title>欢迎使用本系统</Typography.Title>
     </div>
   );
 }
