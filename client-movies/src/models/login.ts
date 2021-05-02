d00e749f7da75a3ff42a766bea36cd7b4e1caf0c
JuneJH <15310487388@163.com>
2021-05-02 23:43:28 +0800

    登录todo

---
+++ client-movies/src/models/login.ts
@@ -0,0 +1,13 @@
+export default {
+    state:null,
+    reducers:{
+        logout(state:any,aciton:any){
+            return aciton.payloay;
+        }
+    },
+    effects:{
+        login(aciton:any,saga:any):any{
+            // 处理登陆的事情
+        }
+    }
+}
